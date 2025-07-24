f3f63427be95041117c51c89df661a7baec9f2e6
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-07-24 23:56:20 +0530

    Time: 1 ms (99.86%) | Memory: 53.4 MB (28.87%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,13 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int major = 0;
-        for(int i = 0;i<nums.length;i++){
+        int count = 0; //initialised count
+        int major = 0; //intialised a tracker to track majority element.
+        for(int i = 0;i<nums.length;i++){//for loop to iterate the through the loop.
             if(nums[i] == nums[major]){
                 count++;
             }else{
                 count--;
             }
+            //as soon as count goes less than 0, we switch the tracker as that elemnt is now the majority element
             if(count<0){
                 major = i;
                 count++;
