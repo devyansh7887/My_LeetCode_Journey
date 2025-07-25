57dbc515ba8a4a2b0651f337d56fe862198da1d1
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-07-25 23:41:17 +0530

    Time: 3 ms (91.58%) | Memory: 45.3 MB (6.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
     if (s.length() != t.length()) return false;
-
+    //if length of both strings are different, then they cant be anagrams.
     char[] a1 = s.toCharArray();
     char[] a2 = t.toCharArray();
-
-    Arrays.sort(a1);
-    Arrays.sort(a2);
-
+    //converting both strings into arrays.
+    Arrays.sort(a1);//worthy
+    Arrays.sort(a2);//worthy
+    //sorting both arrays.
     return Arrays.equals(a1, a2);
     }
 }
