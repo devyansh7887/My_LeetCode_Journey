e068dcd42d50972732756e99222384228c38feef
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-07-30 02:39:06 +0530

    Time: 1 ms (29.08%) | Memory: 42.7 MB (8.46%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,9 +1,11 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // <------- direct approach ------->
         for(int i = m; i<m + n;i++){
             nums1[i] = nums2[i-m]; // randomly puuting the elements of the second array in first
         }
-        Arrays.sort(nums1);
+        Arrays.sort(nums1); 
+        // <---brute force--->
         // for(int x = 0;x<m+n-1;x++){ //traverses throught the entire larger array
         //     for(int y = 0;y<m+n-x-1;y++){ //traverses through the rest unsorted array
         //         if(nums1[y]>nums1[y+1]){
