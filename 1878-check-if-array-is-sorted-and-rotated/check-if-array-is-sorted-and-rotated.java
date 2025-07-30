802450e2baf6b78b053c926b7d5a5bf07abb6e1f
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-07-31 03:13:37 +0530

    Time: 1 ms (7.05%) | Memory: 41.7 MB (10.94%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean check(int[] nums) {
-        int n = nums.length;
-        int count = 0;
+        int n = nums.length; //Store the length of the array
+        int count = 0; //Count to mark the point of deflections, to mark rotations 
         for (int i = 0; i < n - 1; i++) {
             if (nums[i] > nums[i + 1])
                 count++;
         }
+        //Edge case
         if (nums[n - 1] > nums[0]) {
             count++;
         }
-        int[] temp = new int[nums.length];
-        System.arraycopy(nums,0,temp,0,nums.length);
+        // To handle the case of array being already sorted, or case of equal elements
+        int[] temp = new int[n];
+        System.arraycopy(nums,0,temp,0,n);
         Arrays.sort(temp);
         if(Arrays.equals(nums,temp)) return true;
         return (count == 1);
