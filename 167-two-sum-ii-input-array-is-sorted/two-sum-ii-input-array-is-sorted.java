3d335faab89baed5c7c2fa3aa61b7ac64d5e8602
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-07-31 02:13:34 +0530

    Time: 3 ms (23.57%) | Memory: 47.1 MB (80.11%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -6,12 +6,15 @@ class Solution {
         int left = 0;//first index
         int right = numbers.length - 1;//last index
         while(left < right){
+            //Shift the right pointer to left if sum is greater as we would want to reduce it
             if(numbers[left] + numbers[right] > target) right--;
+            //If the target matches return the elements
             if(numbers[left] + numbers[right] == target){
-                arr[0] = left + 1;
-                arr[1] = right + 1;
+                arr[0] = left + 1; //As it is 1-Indexed array
+                arr[1] = right + 1; //As it is 1-Indexed array
                 break;
             }
+            //Shift the left pointer to right if sum is smaller as we would want to increase it
             if(numbers[left] + numbers[right] < target) left++;
         }
         return arr;
