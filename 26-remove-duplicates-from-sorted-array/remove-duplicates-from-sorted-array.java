435eafbfd6a5a3ba71a5f35f769ddceea7f9d6af
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-07-31 22:43:15 +0530

    Time: 18 ms (5.51%) | Memory: 45 MB (40.66%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        // Initial approach - Make a new arrayList, copy all unique elements to it, and then trace it back to original array.
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            if(list.contains(nums[i])) continue; // To check wheather a element is present in the ArrayList.
+            list.add(nums[i]);
+        }
+        // Add the Elements back to the array
+        for(int i = 0; i < list.size(); i++){
+            nums[i] = list.get(i);
+        }
+        // Return the length of the arraylist
+        return list.size();
+    }
+}
