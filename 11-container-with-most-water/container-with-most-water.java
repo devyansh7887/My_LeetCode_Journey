1f1a33d279a49e81424314675777166b21433a1e
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-10-04 23:48:41 +0530

    Time: 5 ms (77.27%) | Memory: 58.1 MB (44.73%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,18 +1,33 @@
 class Solution {
     public int maxArea(int[] height) {
-        int n = height.length;
+        int left = 0;                     // Start pointer
+        int right = height.length - 1;    // End pointer
         int maxArea = 0;
-        int left = 0;
-        int right = n - 1;
-        while(left < right){
-            int area = Math.min(height[left], height[right]) * (right - left);
+
+        while (left < right) {
+            // Calculate current area
+            int h = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+
+            // Update max area if larger
             maxArea = Math.max(maxArea, area);
-            if(height[left] < height[right]){
+
+            // Move the pointer with smaller height
+            if (height[left] < height[right]) {
                 left++;
-            }else{
+            } else {
                 right--;
             }
         }
+
         return maxArea;
+    }
+
+    // For local testing
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.maxArea(new int[]{1,8,6,2,5,4,8,3,7})); // 49
+        System.out.println(sol.maxArea(new int[]{1,1}));               // 1
     }
 }
