790a9d8e763015dbe1cc81800ed82aaf4af4c80a
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2025-10-06 00:50:02 +0530

    Time: 4 ms (97.67%) | Memory: 46.2 MB (11.38%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (heights == null || heights.length == 0) return result;
+
+        int m = heights.length, n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+
+        // DFS from Pacific (top row & left col)
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pacific, i, 0, Integer.MIN_VALUE);
+            dfs(heights, atlantic, i, n - 1, Integer.MIN_VALUE);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(heights, pacific, 0, j, Integer.MIN_VALUE);
+            dfs(heights, atlantic, m - 1, j, Integer.MIN_VALUE);
+        }
+
+        // Collect intersection
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    result.add(Arrays.asList(i, j));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(int[][] heights, boolean[][] visited, int r, int c, int prevHeight) {
+        int m = heights.length, n = heights[0].length;
+        if (r < 0 || c < 0 || r >= m || c >= n) return;
+        if (visited[r][c]) return;
+        if (heights[r][c] < prevHeight) return; // can only flow uphill or equal in reverse
+
+        visited[r][c] = true;
+        dfs(heights, visited, r + 1, c, heights[r][c]);
+        dfs(heights, visited, r - 1, c, heights[r][c]);
+        dfs(heights, visited, r, c + 1, heights[r][c]);
+        dfs(heights, visited, r, c - 1, heights[r][c]);
+    }
+}
