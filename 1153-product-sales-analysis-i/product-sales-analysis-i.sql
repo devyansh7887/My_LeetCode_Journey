7048686ac4a80414410e2e1c0b37a84d59340d09
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2026-02-20 13:42:14 +0530

    Time: 1508 ms (12.22%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
+# Write your MySQL query statement below
 select product_name, year, price from sales left join product
 ON sales.product_id = product.product_id;
