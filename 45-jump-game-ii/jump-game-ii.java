f585b7b3cfbebd70795d871bef7ef15ae4e637f4
Devyansh Upadhyay <dupadhyay7887@gmail.com>
2026-03-18 00:37:23 +0530

    Time: 1 ms (99.60%) | Memory: 47.6 MB (5.79%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -6,8 +6,8 @@ class Solution {
             if (i == currentEnd) {
                 jumps++;
                 currentEnd = farthest;
-            }//
-        }//
+            }
+        }
         return jumps;
     }
     ////
